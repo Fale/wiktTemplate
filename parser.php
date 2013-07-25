8587c2294ea76b51a2aeee1c46b9662c11ae370f
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-07-25 20:08:49 +0000

    Create the query and exec it

--- parser.php
+++ parser.php
@@ -17,6 +17,49 @@ $page = $wiki->getpage("Wikizionario:Bot/Sostituzioni/Template");
 preg_match_all("/<syntaxhighlight.*>(.*)<\/syntaxhighlight>/ims", $page, $matches);
 $content = $matches[1][0];
 
-if( preg_match_all("/\{\{/i", $content))
-	echo "ok\n";
-//print_r($matches);
+preg_match_all("/'\\\{\\\{(.*)'/iU", $content, $matches);
+$clean = Array();
+$jump = 0;
+foreach ($matches[1] as $match)
+{
+	// Remove escaping, pipes and spaces
+	$match = str_replace("\|", "", $match);
+	$match = str_replace("\-", "-", $match);
+	$match = str_replace(" ", "_", $match);
+
+	// Look for (text|text) regex
+	if (preg_match_all("/\(/i", $match))
+	{
+		preg_match_all("/(.*)\((.*)\)(.*)/i", $match, $m);
+		foreach (explode("|", $m[2][0]) as $char) {
+			array_push($clean, $m[1][0] . $char . $m[3][0]);
+		}
+		$jump = 1;
+	}
+	// Look for [Tt] regex
+	// TODO: Make possible to have more than once of these
+	if (preg_match_all("/\[/i", $match))
+	{
+		preg_match_all("/(.*)\[(.*)\](.*)/i", $match, $m);
+		foreach (str_split($m[2][0]) as $char) {
+			array_push($clean, $m[1][0] . $char . $m[3][0]);
+		}
+		$jump = 1;
+	}
+	if ($jump == 0)
+		array_push($clean, $match);
+	else
+		$jump = 0;
+}
+
+// Prepare the query and save in a file
+$q  = " CONNECT itwiktionary_p itwiktionary.labsdb;
+  SELECT DISTINCT concat(\"# [[\", page.page_title, \"]]\")
+  FROM page JOIN templatelinks ON page.page_id = templatelinks.tl_from
+  WHERE templatelinks.tl_title IN ('" . implode("', '", $clean) . "')
+    AND page.page_namespace = 0
+  ORDER BY page.page_title;";
+file_put_contents("query.sql", $q);
+
+// Run the query
+shell_exec("mysql --defaults-file=~/replica.my.cnf -h itwiktionary.labsdb -BN < query.sql > output.out");
